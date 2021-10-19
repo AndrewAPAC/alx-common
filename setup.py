8d10570c78369bd271a8c131f12ae9c5196b6811
Andrew Lister <a.lister.hk@gmail.com>
2021-10-20 07:55:17 +0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='alx-common',
-    version='0.9.3',
+    version='0.9.4',
     author='Andrew Lister',
     author_email='a.lister.hk@gmail.com',
     description='A common library for home development',
