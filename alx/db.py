c4467dfce220f8c477f0083da919ad34256fb68c
Andrew Lister <a.lister.hk@gmail.com>
2022-03-02 15:47:08 +0800

    lint

--- alx/db.py
+++ alx/db.py
@@ -1,14 +1,15 @@
-from sqlalchemy import create_engine, MetaData, Table
-from sqlalchemy.orm import mapper, sessionmaker
-from alx.misc import singleton
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+# from alx.misc import singleton
 from alx.app import logger as logging
+
 
 # @singleton
 class ALXDatabaseSA:
-    def __init__(self, type=None, user=None, passwd=None,
+    def __init__(self, dbtype=None, user=None, passwd=None,
                  host=None, database=None, debug=False):
         connectstr = "%s://%s:%s@%s/%s" % \
-                     (type, user, passwd, host, database)
+                     (dbtype, user, passwd, host, database)
         logging.debug("Connecting to {}:{} as {}".format(host, database, user))
 
         self.engine = create_engine(connectstr,
@@ -18,4 +19,3 @@ class ALXDatabaseSA:
         self.metadata = MetaData(self.engine)
         session = sessionmaker(bind=self.engine)
         self.session = session()
-
