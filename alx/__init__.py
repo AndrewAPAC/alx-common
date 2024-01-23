e4aed0270e0dc7a7067e64d8f37a54a11b93078f
Andrew Lister <a.lister.hk@gmail.com>
2024-01-23 08:36:05 +0800

    date_subst reused when parameter on subsequent calls

--- alx/__init__.py
+++ alx/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.6.2"
+__version__ = "1.6.3"
 __author__ = "Andrew Lister"
 __author_email__ =  "andrew.lister@outlook.co.id"
--- alx/date_util.py
+++ alx/date_util.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import arrow
 
 
-def date_subst(format, when=datetime.now()):
+def date_subst(format, when=None):
     """
     Return a formatted string of the date.
 
@@ -10,6 +10,9 @@ def date_subst(format, when=datetime.now()):
     :param when: the date to convert - default is now
     :return: the date as a formatted string
     """
+    if not when:
+        when = datetime.now()
+
     if type(when) == arrow.Arrow:
         when = when.datetime
 
@@ -17,8 +20,14 @@ def date_subst(format, when=datetime.now()):
 
 
 if __name__ == "__main__":
+    import time
     print(date_subst("%Y-%m-%d"))
     print(date_subst("%a, %b %d %Y"))
     print(date_subst("%H:%M:%S"))
+    time.sleep(2)
+    print(date_subst("%H:%M:%S"))
+    time.sleep(1)
+    print(date_subst("%H:%M:%S"))
+
     a = arrow.get(2013, 5, 5)
     print(date_subst("%a, %b %d %Y", a))
